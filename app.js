948b0c71b870c5f84fc5fed55961f2b27341fd50
Tomio Koshikawa <tvk02jr@gmail.com>
2022-05-16 05:37:05 +0800

    add userManagement

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Parsing Middleware
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Parse application/json
+app.use(bodyParser.json());
+
+// Static Files
+app.use(express.static('public'));
+
+// Templating Engine
+const handlebars = exphbs.create({ extname: '.hbs' });
+app.engine('hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+
+// Connection Pool
+const pool = mysql.createPool({
+  connectionLimit: 500,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+// Connect to DB
+pool.getConnection((err, connection) => {
+  if (err) throw err;
+  console.log(`Connected as ID ${connection.threadId}`);
+});
+
+// Router
+const routes = require('./server/routes/user');
+app.use('/', routes);
+
+app.listen(port, () => console.log(`Server is running at port ${port}.`));
